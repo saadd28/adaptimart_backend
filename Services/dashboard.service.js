8e755b89ee7ba8487b68daf9f0d4d66ffdb59924
Saad Khan <72255519+saadd28@users.noreply.github.com>
2023-12-13 05:32:05 +0500

    Dashboard APIs Integrated

--- Services/dashboard.service.js
+++ Services/dashboard.service.js
@@ -52,7 +52,7 @@ module.exports = {
     },    
     gettopsellingskus: (callBack) => {
       pool.query(
-        "SELECT distinct p.id AS product_id, p.image as image,  p.name AS product_name, p.price AS price, SUM(op.quantity) OVER (PARTITION BY p.id) AS total_quantity_sold, SUM(op.quantity * p.price) OVER (PARTITION BY p.id) AS total_amount_sold FROM product p JOIN order_product op ON p.id = op.product_id JOIN `order` o ON op.order_id = o.id order by total_quantity_sold desc limit 2",
+        "SELECT distinct p.id AS product_id, p.image as image,  p.name AS product_name, p.price AS price, SUM(op.quantity) OVER (PARTITION BY p.id) AS total_quantity_sold, SUM(op.quantity * p.price) OVER (PARTITION BY p.id) AS total_amount_sold FROM product p JOIN order_product op ON p.id = op.product_id JOIN `order` o ON op.order_id = o.id order by total_quantity_sold desc limit 10",
       
         (error, results) => {
           if (error) {
